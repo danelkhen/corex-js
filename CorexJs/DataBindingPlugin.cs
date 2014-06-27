50965fa83d774e04506008f90e8651db146b44f1
danelkhen <danelkhen@gmail.com>
2014-06-27 13:19:17 +0200

    Make databinding extensible - move implementation to Binder

--- CorexJs/DataBindingPlugin.cs
+++ CorexJs/DataBindingPlugin.cs
@@ -1,4 +1,5 @@
-﻿using SharpKit.Html;
+﻿using System;
+using SharpKit.Html;
 using SharpKit.JavaScript;
 using SharpKit.jQuery;
 
@@ -20,6 +21,30 @@ namespace CorexJs
         }
 
 
+        static void element_setup_default(Event e)
+        {
+            var bindings = getBindings(e.target);
+            if (bindings != null)
+            {
+                var binders = bindings.map(t => new Binder(new BinderOptions { SourcePath = t.SourcePath, TargetPath = t.TargetPath }));
+                var target = new jQuery(e.target);
+                target.data("_binders", binders);
+                binders.forEach(t => t.init(e));
+            }
+        }
+
+        static void element_teardown_default(Event e)
+        {
+            var target = new jQuery(e.target);
+            var binders = target.data("_binders").As<JsArray<Binder>>();
+            if (binders != null)
+            {
+                binders.forEach(t => t.destroy(e));
+                target.removeData("_binders");
+            }
+
+        }
+
         static void element_databind_default(Event e)
         {
             //console.log(e.type, e.target.nodeName, e.target.className, JSON.stringify(J(e.target).data("source")));
@@ -27,13 +52,24 @@ namespace CorexJs
             var dataSource = target.data("source");
             var dataMember = target.data("member").As<JsString>();
 
-            //triggerAttributeEvent(e, "onbind", source, member);
-            //if (e.isDefaultPrevented())
-            //    return;
+            var isInited = target.data("databind-isinited").ExactEquals(true);
+            if (!isInited)
+                triggerDataBindingEvent(target, "setup", "onsetup", element_setup_default);
 
-            var bindings = getBindings(e.target);
-            if (bindings != null)
-                databind_bind(dataSource, e.target, bindings);
+
+            var binders = target.data("_binders").As<JsArray<Binder>>();
+            if (binders != null)
+            {
+                binders.forEach(t => t.databind(e));
+            }
+
+            ////triggerAttributeEvent(e, "onbind", source, member);
+            ////if (e.isDefaultPrevented())
+            ////    return;
+
+            //var bindings = getBindings(e.target);
+            //if (bindings != null)
+            //    databind_bind(dataSource, e.target, bindings);
 
             var children = target.children(":not(.Template)");
 
@@ -59,109 +95,22 @@ namespace CorexJs
             var target = J(e.target);
             var dataSource = target.data("source");
 
-            var bindings = getBindings(e.target);
-            if (bindings != null)
-                databind_bindBack(dataSource, e.target, bindings);
+            var binders = target.data("_binders").As<JsArray<Binder>>();
+            if (binders != null)
+            {
+                binders.forEach(t => t.databindback(e));
+            }
+            //var bindings = getBindings(e.target);
+            //if (bindings != null)
+            //    databind_bindBack(dataSource, e.target, bindings);
 
             target.children(":not(.Template)").databindback();
         }
 
-        static void databind_bind(object source, object target, JsArray<Binding> bindings)
-        {
-            if (bindings == null || target == null || source == null)
-                return;
-            bindings.forEach(t =>
-            {
-                if (t.TargetPath == "children")
-                    bindArrayToChildren(target, null, source.tryGetByPath(t.SourcePath));
-                else
-                    databind_tryCopy(source, t.SourcePath, target, t.TargetPath);
-            });
-        }
-
-        static void databind_bindBack(object source, object target, JsArray<Binding> bindings)
-        {
-            bindings.forEach(t =>
-            {
-                if (t.TargetPath == "children")
-                {
-                }
-                else
-                {
-                    databind_tryCopy(target, t.TargetPath, source, t.SourcePath);
-                }
-            });
-        }
-
-        static void databind_tryCopy(object source, JsString sourcePath, object target, JsString targetPath)
-        {
-            var value = source.tryGetByPath(sourcePath);
-            JsObjectExt.trySet(target, targetPath, value);
-        }
-
-        static void bindArrayToChildren(object target, object template, object source)
-        {
-            var list = source.As<JsArray<object>>();
-            var el2 = J(target);
-            var template2 = J(template);
-            if (template2.length == 0)
-                template2 = el2.find(".Template:first");
-            if (template2.length == 0)
-                return;
-            if (list == null)
-                list = el2.data("source").As<JsArray<object>>();
-            if (!(list is JsArray<object>))
-                return;
-            var children = el2.children(":not(.Template)").toArray();
-
-            JsFunc<object, jQuery> createTemplate = t => template2.clone(true).removeClass("Template").data("source", t);
-
-            bindArrayToChildrenInternal(list, el2, children, createTemplate);
-        }
-
-        static void bindArrayToChildrenInternal(JsArray<object> source, jQuery target, JsArray<HtmlElement> children, JsFunc<object, jQuery> creator)
-        {
-            var index = 0;
-            var index2 = 0;
-            while (index2 < children.length)
-            {
-                var ch2 = J(children[index2]);
-                var dc2 = ch2.data("source");
-                if (dc2 == null)
-                {
-                    index2++;
-                    continue;
-                }
-                var dc = source[index];
-                if (dc != dc2)
-                {
-                    if (dc == null)
-                    {
-                        ch2.remove();
-                        index2++;
-                        continue;
-                    }
-                    else
-                    {
-                        var ch3 = creator(dc);
-                        ch3.insertBefore(ch2);
-                        index++;
-                        continue;
-                    }
-                }
-                index2++;
-                index++;
-            }
-            while (index < source.length)
-            {
-                target.append(creator(source[index]));
-                index++;
-            }
-        }
 
         static JsArray<Binding> getBindings(HtmlElement el)
         {
-            var bindings = parseBindings(J(el).data("bindings").As<JsString>(), getDefaultBindingTarget(el));
+            var bindings = parseBindings(J(el).data("bindings").As<JsString>(), getDefaultTargetPath(el));
             return bindings;
         }
 
@@ -186,7 +135,7 @@ namespace CorexJs
             return list;
         }
 
-        static JsString getDefaultBindingTarget(HtmlElement el)
+        public static JsString getDefaultTargetPath(HtmlElement el)
         {
             if (el.nodeName == "INPUT")
             {
@@ -243,6 +192,134 @@ namespace CorexJs
     }
 
 
+    [JsType(JsMode.Prototype, Name ="Binder", Filename ="res/databind.js")]
+    class Binder
+    {
+        public Binder(BinderOptions options)
+        {
+            Options = options;
+        }
+        BinderOptions Options;
+
+        public virtual void init(Event e)
+        {
+            if (Options.TargetPath == null)
+                Options.TargetPath = DataBindingPlugin.getDefaultTargetPath(e.target);
+            if (Options.BindBackOn != null && Options.BindBackOn.length > 0)
+            {
+                var target = new jQuery(e.target);
+                target.on(Options.BindBackOn, databindback);
+            }
+
+        }
+
+        public virtual void databind(Event e)
+        {
+            var target = new jQuery(e.target);
+            var source = target.data("source");
+            if (Options.TargetPath == "children")
+                bindArrayToChildren(target, null, source.tryGetByPath(Options.SourcePath));
+            else
+                databind_tryCopy(source, Options.SourcePath, e.target, Options.TargetPath);
+        }
+
+        public virtual void databindback(Event e)
+        {
+            if (Options.OneWay)
+                return;
+            if (Options.TargetPath == "children")
+                return;
+            var target = new jQuery(e.target);
+            var source = target.data("source");
+            databind_tryCopy(e.target, Options.TargetPath, source, Options.SourcePath);
+        }
+
+        public virtual void destroy(Event e)
+        {
+            if (Options.BindBackOn != null && Options.BindBackOn.length > 0)
+            {
+                var target = new jQuery(e.target);
+                target.off(Options.BindBackOn, databindback);
+            }
+        }
+
+        static void databind_tryCopy(object source, JsString sourcePath, object target, JsString targetPath)
+        {
+            var value = source.tryGetByPath(sourcePath);
+            JsObjectExt.trySet(target, targetPath, value);
+        }
+
+        static void bindArrayToChildren(object target, object template, object source)
+        {
+            var list = source.As<JsArray<object>>();
+            var el2 = new jQuery(target);
+            var template2 = new jQuery(template);
+            if (template2.length == 0)
+                template2 = el2.find(".Template:first");
+            if (template2.length == 0)
+                return;
+            if (list == null)
+                list = el2.data("source").As<JsArray<object>>();
+            if (!(list is JsArray<object>))
+                return;
+            var children = el2.children(":not(.Template)").toArray();
+
+            JsFunc<object, jQuery> createTemplate = t => template2.clone(true).removeClass("Template").data("source", t);
+
+            bindArrayToChildrenInternal(list, el2, children, createTemplate);
+        }
+
+        static void bindArrayToChildrenInternal(JsArray<object> source, jQuery target, JsArray<HtmlElement> children, JsFunc<object, jQuery> creator)
+        {
+            var index = 0;
+            var index2 = 0;
+            while (index2 < children.length)
+            {
+                var ch2 = new jQuery(children[index2]);
+                var dc2 = ch2.data("source");
+                if (dc2 == null)
+                {
+                    index2++;
+                    continue;
+                }
+                var dc = source[index];
+                if (dc != dc2)
+                {
+                    if (dc == null)
+                    {
+                        ch2.remove();
+                        index2++;
+                        continue;
+                    }
+                    else
+                    {
+                        var ch3 = creator(dc);
+                        ch3.insertBefore(ch2);
+                        index++;
+                        continue;
+                    }
+                }
+                index2++;
+                index++;
+            }
+            while (index < source.length)
+            {
+                target.append(creator(source[index]));
+                index++;
+            }
+        }
+
+
+    }
+
+    [JsType(JsMode.Json)]
+    class BinderOptions
+    {
+        public bool OneWay { get; set; }
+        public JsString SourcePath { get; set; }
+        public JsString TargetPath { get; set; }
+        public JsString BindBackOn { get; set; }
+    }
 
     [JsType(JsMode.Json)]
     class Binding
@@ -274,3 +351,53 @@ namespace CorexJs
     }
 
 }
+
+
+//[JsType(JsMode.Prototype, Name="BinderHelper", Filename ="res/databind.js")]
+//class BinderHelper
+//{
+//    public static Binder twoWay(JsString sourcePath, JsString targetPath)
+//    {
+//        return new Binder
+//        {
+//            databind = e =>
+//            {
+//                var target = new jQuery(e.target);
+//                var source = target.data("source");
+//                DataBindingPlugin.databind_tryCopy(source, sourcePath, e.target, targetPath);
+//            },
+//            databindback = e =>
+//            {
+//                var target = new jQuery(e.target);
+//                var source = target.data("source");
+//                DataBindingPlugin.databind_tryCopy(e.target, targetPath, source, sourcePath);
+//            }
+//        };
+//    }
+//}
+//static void databind_bind(object source, object target, JsArray<Binding> bindings)
+//{
+//    if (bindings == null || target == null || source == null)
+//        return;
+//    bindings.forEach(t =>
+//    {
+//        if (t.TargetPath == "children")
+//            bindArrayToChildren(target, null, source.tryGetByPath(t.SourcePath));
+//        else
+//            databind_tryCopy(source, t.SourcePath, target, t.TargetPath);
+//    });
+//}
+
+//static void databind_bindBack(object source, object target, JsArray<Binding> bindings)
+//{
+//    bindings.forEach(t =>
+//    {
+//        if (t.TargetPath == "children")
+//        {
+//        }
+//        else
+//        {
+//            databind_tryCopy(target, t.TargetPath, source, t.SourcePath);
+//        }
+//    });
+//}
