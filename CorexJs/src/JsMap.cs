5560e35cce1fd13f1aace3fbec0702ea2a869b52
danelkhen <danelkhen@gmail.com>
2015-09-03 14:44:28 +0200

    Add JsMap definition

---
+++ CorexJs/src/JsMap.cs
@@ -0,0 +1,25 @@
+﻿
+namespace SharpKit.JavaScript
+{
+    [JsType(JsMode.Prototype, Name = "Map", Export = false)]
+    public class JsMap<K, T>
+    {
+        public JsNumber size { get; private set; }
+        ///Removes all elements from a map.
+        public void clear() { }
+        ///Removes a specified element from a map.
+        public void delete(K key) { }
+        ///Performs the specified action for each element in a map.
+        public void forEach(JsAction<T, K> cb) { }
+        ///Returns a specified element from a map.
+        public T get(K key) { return default(T); }
+        ///Returns true if the map contains a specified element.
+        public bool has(K key) { return false; }
+        ///Adds a new element to a map.
+        public void set(K key, T value) { }
+        ///Returns a string representation of a map.
+        public JsString toString() { return null; }
+        ///Returns the primitive value of the specified object.
+        public object valueOf() { return null; }
+    }
+}
