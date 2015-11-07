5f825a87c51dfcb452d994d21a150c42fa53c59c
danelkhen <danelkhen@gmail.com>
2015-11-07 12:34:35 +0100

    g

---
+++ amp/tests/test2/compiler.js
@@ -0,0 +1,189 @@
+﻿"use strict"
+function Compiler() {
+    var _this = this;
+    Function.addTo(_this, [parse, parseLines, generate, compile]);
+    function isValidIdentifier(s) {
+        return /^[a-zA-Z_]+[a-zA-Z0-9]*$/.test(s);
+    }
+
+    function genImportExport(name, obj) {
+        var keys = Object.keys(obj).where(isValidIdentifier);
+        if (keys.length == 0)
+            null;
+        var x = {};
+        x.imp = "var " + keys.select(function (key) { return key + "=" + name + "." + key }).join(",") + ";";
+        x.exp = keys.select(function (key) { return name + "." + key + "=" + key + ";" }).join("\n");
+        return x;
+    }
+
+    function compileWithContext(expr, ctx, oneWay) {
+        var ie = genImportExport("__ctx", ctx);
+        //TODO: verify all keys are legit vars
+        var keys = Object.keys(ctx).where(isValidIdentifier);
+        var code = [];
+        if (keys.length > 0)
+            code.push(ie.imp);
+        code.push("var __res = \n" + expr + ";");
+        if (!oneWay)
+            code.push(ie.exp);
+        code.push("return __res;");
+        var code2 = code.join("\n");
+        var func = new Function("__ctx", code2);
+        return func;
+    }
+
+    function compile(code, ctx) {
+        if (ctx == null)
+            return new Function("return " + code);
+        var func = compileWithContext(code, ctx);
+        return func;
+    }
+    function generate2(node, nodeText, tab) {
+        var hasChildren = node.children != null && node.children.length > 0;
+        if (hasChildren) {
+            var s = tab + "APPEND(" + nodeText + ", [\n"
+            s += node.children.select(generate).join(", \n");
+            s += tab + "])"
+            return s;
+        }
+        var s = tab + nodeText;
+        return s;
+    }
+
+    _this.import = function (name) {
+        var obj = window[name];
+        return genImportExport(name, obj).imp;
+    }
+
+    function generate(node) {
+        if (node.text.trim().startsWith("COMPILER.")) {
+            var ctx ={ COMPILER: _this }; 
+            var code = compileWithContext(node.text, ctx)(ctx);
+            return code;
+        }
+        var func = FunctionHelper.parse(node.text);
+        var hasChildren = node.children != null && node.children.length > 0;
+        var nodeText = node.text;
+        var tab = "".padRight(node.tab, " ");
+        if (func != null && hasChildren & func.prms != null && func.prms.length > 0) {
+            var prms = func.prms.join(", ");
+            if (func.prms.length > 1)
+                prms = "(" + prms + ")";
+            var s = tab + prms + " => ";
+            nodeText = func.body.trim();
+            s += generate2(node, nodeText, "");
+            return s;
+        }
+        return generate2(node, node.text, tab);
+    }
+
+    function parse(markup, parent) {
+        var lines = markup.lines();
+        var nodes = parseLines(lines);
+        return nodes;
+    }
+
+    function parseLines(_lines) {
+        var stack = [{ children: [], tab: -1 }];
+
+        _lines.forEach(function (line) {
+            var tab = getTab(line);
+            var node = { text: line.substr(tab), tab: tab, children: [] };
+            var prev = stack.last();
+            var prevTab = prev.tab;
+            if (tab > prevTab) {
+                prev.children.push(node);
+                stack.push(node);
+            }
+            else if (tab == prevTab) {
+                stack.pop();
+                prev = stack.last();
+                prevTab = prev.tab;
+                prev.children.push(node);
+                stack.push(node);
+            }
+            else {
+                while (tab <= prevTab) {
+                    stack.pop();
+                    prev = stack.last();
+                    prevTab = prev.tab;
+                }
+                prev.children.push(node);
+                stack.push(node);
+            }
+        });
+        return stack[0].children;
+
+        function getTab(s) {
+            return s.search(/\S/);
+        }
+        function append(parent, child) {
+            return s.search(/\S/);
+        }
+
+    }
+}
+
+
+function FunctionHelper() {
+    Function.addTo(FunctionHelper, [parse]);
+    function parse(s) {
+        var prms = parseArrowFunctionArgNames(s);
+        if (prms != null) {
+            var arrowEnd = s.indexOf("=>") + 2;
+            var body = s.substr(arrowEnd);
+            var type = "ArrowExpressionFunction";
+            if (body.trim().startsWith("{"))
+                type = "ArrowFunction";
+
+            return { body: body, prms: prms, type: type, name: null };
+        }
+        prms = parsePrms(s);
+        if (prms == null)
+            return null;
+        var body = s.substring(s.indexOf("{") + 1, s.lastIndexOf("}") - 1);
+        var name = s.substringBetween("function ", "(").trim();
+        var type = name == "" ? "AnonymousFunction" : "NamedFunction";
+        return { body: body, prms: prms, type: type, name: name };
+    }
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var FN_ARG_SPLIT = /,/;
+    var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+    var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    function parsePrms(s) {
+        var list = [];
+        var fnText = s.toString().replace(STRIP_COMMENTS, '');
+        var argDecl = fnText.match(FN_ARGS);
+        if (argDecl == null)
+            return null;
+        argDecl[1].split(FN_ARG_SPLIT).forEach(function (arg) {
+            arg.replace(FN_ARG, function (all, underscore, name) {
+                list.push(name);
+            });
+        });
+        return list;
+    }
+    function isValidIdentifier(s) {
+        return /^[a-zA-Z_]+[a-zA-Z0-9]*$/.test(s);
+    }
+
+    function parseArrowFunctionArgNames(s) {
+        var index = s.indexOf("=>");
+        if (index <= 0)
+            return null;
+        var sub = s.substr(0, index).trim();
+        if (sub.startsWith("(") && sub.endsWith(")")) {
+            var sub2 = sub.substr(1, sub.length - 2).trim();
+            if (sub2 == "")
+                return [];
+            var tokens = sub2.split(',').selectInvoke("trim");
+            if (tokens.all(isValidIdentifier))
+                return tokens;
+            return null;
+        }
+        if (isValidIdentifier(sub))
+            return [sub];
+        return null;
+    }
+}
+FunctionHelper();
